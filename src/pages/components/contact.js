21dbb137b80a74e4fc2b9f7564d74d2a89d9a211
leonelVc19 <vazquez.juan.1fm@gmail.com>
2023-04-11 23:06:21 -0600

    Mis proyectos

--- src/pages/components/contact.js
+++ src/pages/components/contact.js
@@ -1,38 +1,28 @@
 import styles from '@/styles/Contact.module.css';
-import Image from 'next/image';
 import ContactComponent from './components/ContactComponent';
+import NavBar from '../NavBar/nav';
+import { socialNetworks } from '../../../public/data/socialNetwork';
 function Contact() {
     return (
-        <nav>
-            <ul className={styles.lista}>
-                <ContactComponent 
-                    titulo="LinkedIn" 
-                    imagen="/icons/linkedin.png" 
-                    url="https://www.linkedin.com/in/juan-leonel-vazquez-iglesias-74b553230/"
-                />
-                <ContactComponent 
-                    titulo="Git Hub" 
-                    imagen="/icons/github.png" 
-                    url="https://github.com/leonelVc19" 
-                />
-                <ContactComponent 
-                    titulo="Instagram" 
-                    imagen="/icons/instagram.png" 
-                    url="https://www.instagram.com/jlvi19c" 
-                />
-                <ContactComponent 
-                    titulo="Whatsapp" 
-                    imagen="/icons/whatsapp.png" 
-                    url="https://wa.me/522431219195?text=Hola+Juan+Leonel%21" 
-                />
-                <ContactComponent 
-                    titulo="Email" 
-                    imagen="/icons/gmail.png" 
-                    url="mailto: vazquez.juan.1fm@gmail.com" 
-                />
-
-            </ul>
-        </nav>
+        <>
+            <NavBar />
+            <p>Hola</p>
+            <div className={styles.contact}>
+                
+                <nav className={styles.lista}>
+                    <ul className={styles.lista}>
+                        {socialNetworks.map(social => <>
+                            <ContactComponent 
+                                titulo={social.titulo} 
+                                imagen={social.imagen} 
+                                url={social.url}
+                            />
+                            </>
+                        )}
+                    </ul>
+                </nav>
+            </div>
+        </>
     );
 };
 
