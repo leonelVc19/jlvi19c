3dc50a2f6d6f845e89f98b3aedf5b899b4386021
leonelVc19 <vazquez.juan.1fm@gmail.com>
2023-04-17 00:31:21 -0600

    Correcion de algunos detalles

--- public/data/Skills.js
+++ public/data/Skills.js
@@ -1,71 +1,79 @@
 export const skills = [
     {
         title: 'JavaScript',
-        imagen: '/iconSkills/js.png',
+        imagen: '/iconSkills/javascript.svg',
         url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
     },
     {
+        title: 'HTML',
+        imagen: 'https://lenguajehtml.com/html/logo.svg',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/html'
+    },{
         title: 'CSS',
         imagen: '/iconSkills/css-3.png',
         url: 'https://developer.mozilla.org/en-US/docs/Web/css'
+    },
+    {
+        title: 'Nodejs',
+        imagen: 'https://nodejs.org/static/images/logo.svg',
+        url: 'https://nodejs.org/en/docs'
+    },
+    {
+        title: 'Express',
+        imagen: '/iconSkills/expressjs.svg',
+        url: 'https://expressjs.com/en/starter/installing.html'
+    },
+    {
+        title: 'Git',
+        imagen: '/iconSkills/git.png',
+        url: 'https://git-scm.com/'
 
     },
     {
-        title: 'HTML',
-        imagen: '/iconSkills/HTML.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/html'
+        title: 'Git Hub',
+        imagen: '/icons/github.png',
+        url: 'https://github.com/leonelVc19'
     },
     {
-        title: 'JavaScript',
-        imagen: '/iconSkills/js.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        title: 'React',
+        imagen: '/iconSkills/react.svg',
+        url: 'https://react.dev/learn'
     },
     {
-        title: 'CSS',
-        imagen: '/iconSkills/css-3.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/css'
+        title: 'React N.',
+        imagen: '/iconSkills/react.svg',
+        url: 'https://reactnative.dev/'
+    },
+    {
+        title: 'Expo',
+        imagen: '/iconSkills/logo-wordmark.svg',
+        url: 'https://docs.expo.dev/'
+    },
+    {
+        title: 'Lit',
+        imagen: '/iconSkills/lit.svg',
+        url: 'https://lit.dev/docs/'
 
     },
     {
-        title: 'HTML',
-        imagen: '/iconSkills/HTML.png',
+        title: 'Next JS',
+        imagen: '/iconSkills/next.svg',
         url: 'https://developer.mozilla.org/en-US/docs/Web/html'
     },
     {
-        title: 'JavaScript',
-        imagen: '/iconSkills/js.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
-    },
-    {
-        title: 'CSS',
-        imagen: '/iconSkills/css-3.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/css'
+        title: 'SASS',
+        imagen: 'https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg',
+        url: 'https://sass-lang.com/documentation/'
 
     },
     {
-        title: 'HTML',
-        imagen: '/iconSkills/HTML.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/html'
+        title: 'Firebase',
+        imagen: 'https://www.gstatic.com/devrel-devsite/prod/v07c44c7c1b3fe607b0ea91e300f4cf82edbe2da6334f7664661c6358babdf558/firebase/images/lockup.svg',
+        url: 'https://firebase.google.com/docs'
     },
     {
-        title: 'HTML',
-        imagen: '/iconSkills/HTML.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/html'
+        title: 'MySQL',
+        imagen: '/iconSkills/mysql.svg',
+        url: 'https://dev.mysql.com/doc/'
     },
-    {
-        title: 'JavaScript',
-        imagen: '/iconSkills/js.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
-    },
-    {
-        title: 'CSS',
-        imagen: '/iconSkills/css-3.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/css'
-
-    },
-    {
-        title: 'HTML',
-        imagen: '/iconSkills/HTML.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/html'
-    }
 ]
