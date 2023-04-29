e747d2423bc48bbabaa1af678661214c2d70bd1e
leonelVc19 <vazquez.juan.1fm@gmail.com>
2023-04-28 19:19:37 -0600

    Arreglando el error del deploy

--- src/pages/components/components/Button.js
+++ src/pages/components/components/Button.js
@@ -1,7 +1,7 @@
 import styles from '@/styles/ButtonComponent.module.css';
 import Link from 'next/link';
 
-function Button({title, link}) {
+function Button({title, link = '/'}) {
     return <Link className={styles.informacion_cv_link} href={link}>{title}</Link>;
 };
 
