63c07b29060660a175722058bba608d388de2b3d
leonelVc19 <vazquez.juan.1fm@gmail.com>
2025-01-26 22:48:51 -0600

    Fix codigo de cards en el proyecto

--- src/pages/components/projects/my_projects.js
+++ src/pages/components/projects/my_projects.js
@@ -1,40 +1,14 @@
 import * as React from 'react';
-
 import '@/pages';
 import styles from '@/styles/MyProjects.module.css';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 
-const url_api = process.env.NEXT_PUBLIC_URL_API;
 const MyProjects = () => {
     const  { query: {id}} = useRouter();
     let id_project = id;
-    // const url_p = url === "_" ? '#' : url;
-    // const url_target = url === "_" ? '' : '_blank';
-    const [project, setProject] = React.useState();
     const proyecto = project?.projectByID ?? {};
-    
-    const getProjects = async () => {
-            try {
-                let id_p = id_project;
-                const storedId = localStorage.getItem('id_project_p');
-                if (!id_p && storedId) {
-                    id_p = storedId;
-                    console.log("ID desde el localStorage", id_p);
-                }
-                const response = await fetch(`${url_api}/project/${id_p}`);
-                const findProjectById = await response.json();
-                console.log(findProjectById);
-                setProject(findProjectById);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-    React.useEffect(() => {
-        let id_project = id;
-        localStorage.setItem("id_project_p", String(id_project));
-        getProjects();
-    }, [id]);
+
     
     return (
         <div className={styles.main}>
--- src/pages/components/projects/project.js
+++ src/pages/components/projects/project.js
@@ -6,22 +6,6 @@ import Image from "next/image"
 const url_api = process.env.NEXT_PUBLIC_URL_API;
 
 function Project  () {
-    
-    const [projectsApi, setProjectsApi] = React.useState([]);
-    const get_projects = async () => {
-        try {
-            const response = await fetch(`${url_api}/projects`);
-            const  response_projects = await response.json();
-            setProjectsApi(response_projects);
-        } catch (error) {
-            // console.error(error);
-            setProjectsApi([])
-        }
-    }
-    React.useEffect( () => {
-        get_projects();
-    }, []);
-
     return (
         <section className={styles.section}>
             <header className={styles.center_header}>
@@ -31,44 +15,37 @@ function Project  () {
                 </p>
             </header>
             <section  className={styles.container}>
-                { 
+                <article className={styles.card}>
+                    <Image
+                        src='https://jlvi-iglesias.s3.us-west-1.amazonaws.com/inicio_candy.jpeg'
+                        alt='Candy App'
+                        width={300}
+                        height={300}
+                        className={styles.card_img}
+                    ></Image>
+                    <div className={styles.card_content}>
+                        <h2 className={styles.card_title}>Candy App</h2>
+                        <p className={styles.card_description}>Aplicacion movil para un comercion, desarrollador para la gestion de mercancia en la sucursal.</p>
+                        <div className={styles.content_skills}>
+                            <span className={styles.skill}>ReactNative</span>
+                            <span className={styles.skill}>JS</span>
+                            <span className={styles.skill}>Expo</span>
+                            <span className={styles.skill}>Firebase</span>
+                            <span className={styles.skill}>Scrum</span>
+                            <span className={styles.skill}>Lider de proyecto</span>
+                        </div>
+                        <br></br>
+                        <a href="#" className={styles.card_link}>App movil</a>
+                    </div>
+                </article>
+                {/* { 
                 projectsApi.length === 0 ? 
                     <p>SIN PROYECTOS DE MOMENTO</p> :
                     projectsApi.map((project, index) => {
-                        const url_p = project.url === "" ? null : project.url;
-                        const url_target = project.url === "" ? null : '_blank';
                         return (
-                            <article className={styles.card} key={index}>
-                                <div className={styles.content}>
-                                    <a href={url_p} target={url_target}>
-                                        <Image
-                                            className={styles.content_image}
-                                            src={project.image}
-                                            style={{objectFit: "cover"}}	
-                                            width={280}
-                                            height={200}
-                                            alt={project.title}
-                                            loading="lazy"
-                                        />
-                                    </a>
-                                    <footer className={styles.content_footer}>
-                                        <pre>{project.id}</pre>
-                                        <h3>{project.title}</h3>
-                                        <p className={styles.description}>Una peque;a descricion de algo que este en la car d</p>
-                                        <Link className={styles.content_footer_a} href={{
-                                            pathname: '/components/projects/my_projects',
-                                            query: { 
-                                                id: project.id,
-                                            }
-                                        }}>
-                                            Más Información
-                                        </Link>
-                                    </footer>
-                                </div>
-                            </article>
                         );
                     })
-                }
+                } */}
             </section>
         </section>
     );
