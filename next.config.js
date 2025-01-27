b656ae54be2181a7ef7fcf4350cf6764b7e0ffb4
leonelVc19 <vazquez.juan.1fm@gmail.com>
2025-01-26 21:15:27 -0600

    Fix version Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const domain = process.env.NEXT_PUBLIC_S3_DOMAIN;
-module.exports = {
+const nextConfig = {
+
   reactStrictMode: true,
   swcMinify: true,
   optimizeFonts: true,
@@ -8,9 +8,10 @@ module.exports = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: domain
+        hostname:  process.env.NEXT_PUBLIC_S3_DOMAIN
       }
     ],
     minimumCacheTTL: 1500000,
   },
 }
+export default nextConfig;
